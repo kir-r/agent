9e3a9d4630a8126e80f28c423ac65398a3c6faad
IgorKey <Igor_Kuzminykh@epam.com>
2020-02-14 23:06:56 +0300

    fixed windows compilation

--- src/mingwX64Main/kotlin/com/epam/drill/DynamicLoader.kt
+++ src/mingwX64Main/kotlin/com/epam/drill/DynamicLoader.kt
@@ -1,4 +1,5 @@
 @file:Suppress("unused", "UNUSED_PARAMETER")
+
 package com.epam.drill
 
 import com.epam.drill.plugin.api.processing.*
@@ -7,29 +8,9 @@ import platform.windows.*
 
 
 actual fun injectDynamicLibrary(path: String): Any? = memScoped {
-    LoadLibrary!!(path.replace("/", "\\").toLPCWSTR(this).pointed.ptr)?.GetProcAddress(hModule, initPlugin)
+    LoadLibrary!!(path.replace("/", "\\").toLPCWSTR(this).pointed.ptr)
+        ?.let { hModule -> GetProcAddress(hModule, initPlugin) }
 }
-
-//        val initPlugin =
-
-//        val callbacks: jvmtiEventCallbacks? = gjavaVMGlob?.pointed?.callbackss
-//        val reinterpret =
-//            initPlugin?.reinterpret<CFunction<(CPointer<ByteVar>, CPointer<com.epam.drill.jvmapi.gen.jvmtiEnvVar>?, CPointer<JavaVMVar>?, CPointer<jvmtiEventCallbacks>?, CPointer<CFunction<(pluginId: CPointer<ByteVar>, message: CPointer<ByteVar>) -> Unit>>) -> COpaquePointer>>()
-//        val id = pluginId.cstr.getPointer(this)
-//        val jvmti = gdata?.pointed?.jvmti
-//        val jvm = gjavaVMGlob?.pointed?.jvm
-//        val clb = callbacks?.ptr
-//        pluginInstance =
-//            reinterpret?.invoke(
-//                id,
-//                jvmti,
-//                jvm,
-//                clb,
-//                sender
-//            )?.asStableRef<NativePart<*>>()?.get()
-//    }
-//    pluginInstance
-//}
 
 private fun String.toLPCWSTR(ms: MemScope): CArrayPointer<UShortVar> {
     val length = this.length
